27506f290846b44db1ffbd91159044c08336299b
User <william99113@gmail.com>
2025-11-21 09:31:37 +0800

    First commit

---
+++ src/main/java/training/web/Hello.java
@@ -0,0 +1,9 @@
+package training.web;
+
+public class Hello{ 
+    public static void main(String[] args){
+        System.out.println("Hello Manve Project2");
+    }
+}
+// mvn compile 
+//java -cp ./target/classes training.web.Hello 
